df760fa61f6935ae3a094b7d909f32d4141df7f2
Zeeshan Ali <86999770+Zeeshanunique@users.noreply.github.com>
2024-01-02 22:43:51 +0530

    master

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-load_dotenv()  ##loading all the environment variables
+load_dotenv() ## loading all the environment variables
 
 import streamlit as st
 import os
@@ -7,23 +7,41 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## function to load Gemini Pro model and get responses
-model=genai.GenerativeModel("gemini-pro")
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
 def get_gemini_response(question):
-    response=model.generate_content(question)
-    return response.text
+    
+    response=chat.send_message(question,stream=True)
+    return response
 
-## initialize our streamlit app
+##initialize our streamlit app
 
-st.set_page_config(page_title='Q&A Demo')
+st.set_page_config(page_title="Q&A Demo")
 
-input=st.text_input("Input:",key='input')
+st.header("Rania GPT")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input=st.text_input("Input: ",key="input")
 submit=st.button("Ask the question")
 
-## when submit is click
-
-if submit:
+if submit and input:
     response=get_gemini_response(input)
-    st.subheader("The Response is:")
-    st.write(response)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
+    
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
+    
 
+
+
+    
